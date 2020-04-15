4733d8a406c8ec708bd99c2f269dcda5a96e84d0
LolitaLestari <lolita9a@gmail.com>
2020-04-15 10:37:42 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -1,7 +1,18 @@
 package Operator;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class OperatorBinary {
     String nama = "Lolita Lestari";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[]args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
